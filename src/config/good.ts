e917aa0f83539a1dbbd0b8357de7a63874e3bde6
jcoleman-techempower <jcoleman@techempower.com>
2018-10-02 13:37:14 -0700

    Adds type definition to Good plugin config

--- src/config/good.ts
+++ src/config/good.ts
@@ -1,4 +1,11 @@
-export default {
+import { ServerRegisterPluginObject } from "hapi";
+
+// The generic type of ServerRegisterPluginObject must be
+// of type unknown` because there are no type definitions
+// for the `Good` node library. If at some point those type
+// definitions become available, that type will be used
+// instead.
+export default<ServerRegisterPluginObject<unknown | NodeRequire>> {
   plugin: require("good"),
   options: {
     ops: { interval: 1000 },
