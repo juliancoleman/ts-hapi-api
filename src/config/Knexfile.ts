80954a29a449e316a1d4dfdb1c5336709b981d50
jcoleman-techempower <jcoleman@techempower.com>
2018-10-02 17:05:37 -0700

    Trying to figure out why pg won't connect

--- src/config/Knexfile.ts
+++ src/config/Knexfile.ts
@@ -1,3 +1,5 @@
+// tslint:disable:max-line-length
+
 import { Config, PoolConfig, MigratorConfig, ConnectionConfig } from "knex";
 import * as dotenv from "dotenv";
 import * as pg from "pg";
@@ -10,17 +12,16 @@ numberTypes.forEach((type: number) =>
   pg.types.setTypeParser(type, "text", parseFloat)
 );
 
-const client: string = "postgresql";
+const client: string = "pg";
 const pool: Readonly<PoolConfig> = { min: 2, max: 10 };
 const migrations: Readonly<MigratorConfig> = {
   directory: "../migrations",
-  tableName: "knex_migrations",
   disableTransactions: true,
 };
 const connection: Readonly<ConnectionConfig> = {
   host: process.env.DATABASE_HOST as string,
   user: process.env.DATABASE_USER as string,
-  password: process.env.DATABASE_PASSWORD as string,
+  password: process.env.DATABASE_PASSWORD as string, // this needs infinitely more security around it
   database: process.env.DATABASE_NAME as string,
 };
 
